0249fb146d4d3ee2e7c36ebbc699c7ff84cdd4a3
guruprasad015 <guruprasad015@gmail.com>
2017-05-28 14:15:43 -0700

    Update FindDuplicates.java
    Added steps to find duplicates in an integer array.

--- FindDuplicates.java
+++ FindDuplicates.java
@@ -1,10 +1,15 @@
 package com.guru;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class FindDuplicates {
 
+	/*
+	Prints true if the array has duplicates else false. 
+	Loop though the array and insert each element into an hashmap. Before inserting the next element check if the element
+	is already present in the map. If yes, then return true and if we've looped through all the elements in the array, 
+	then return false. 
+	*/
 	public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<Integer,Integer>();
         if(nums.length ==0)
@@ -22,7 +27,5 @@ public class FindDuplicates {
 		int[] input = {1,2,3,4,5,6,4};
 		FindDuplicates findDup = new FindDuplicates();
 		System.out.println("Input Array has duplicates : " + findDup.containsDuplicate(input));
-
 	}
-
 }
